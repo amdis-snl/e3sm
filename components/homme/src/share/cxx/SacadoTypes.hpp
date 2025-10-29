d5a51a8f5632965acd22f625fe09e14ad9914661
Luca Bertagna <lbertag@sandia.gov>
2025-10-29 14:34:59 -0600

    HOMME: fix sacado unit test

--- components/homme/src/share/cxx/SacadoTypes.hpp
+++ components/homme/src/share/cxx/SacadoTypes.hpp
@@ -8,9 +8,11 @@
 namespace Homme {
 
 // TODO: decide what to do in terms of which Fad to support (SFad, SLFad, DFad, etc)
-template<typename T>
-using SFad = Sacado::Fad::SFad<T,HOMMEXX_SFAD_SIZE>;
+template<typename T,int N>
+using SFadN = Sacado::Fad::SFad<T,N>;
 
+template<typename T>
+using SFad = SFadN<T,HOMMEXX_SFAD_SIZE>;
 }
 
 #endif // HOMMEXX_SACADO_TYPES_HPP
