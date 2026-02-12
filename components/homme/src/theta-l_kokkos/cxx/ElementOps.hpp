3e27860959dd7f5510e95108a318d8b7292cc36b
Luca Bertagna <lbertag@sandia.gov>
2026-02-12 16:47:49 -0700

    Homme: template ElementOps methods on scalar type

--- components/homme/src/theta-l_kokkos/cxx/ElementOps.hpp
+++ components/homme/src/theta-l_kokkos/cxx/ElementOps.hpp
@@ -22,12 +22,12 @@ public:
     m_hvcoord = hvcoord;
   }
 
-  template<typename InputProvider>
+  template<typename InputProvider, typename PT, typename... Props>
   KOKKOS_INLINE_FUNCTION
   void get_R_star (const KernelVariables& kv,
                    const bool use_moisture,
                    const InputProvider& Q,
-                   const ExecViewUnmanaged<Scalar[NUM_LEV]>& R) const {
+                   const ExecView<PT[NUM_LEV],Props...>& R) const {
     using namespace PhysicalConstants;
     if (use_moisture) {
       Kokkos::parallel_for(Kokkos::ThreadVectorRange(kv.team,NUM_LEV),
@@ -42,27 +42,29 @@ public:
     }
   }
 
+  template<typename InputProvider, typename PT, typename... Props>
   KOKKOS_INLINE_FUNCTION
   void compute_hydrostatic_p (const KernelVariables& kv,
-                              const ExecViewUnmanaged<const Scalar[NUM_LEV  ]>& dp,
-                              const ExecViewUnmanaged<      Scalar[NUM_LEV_P]>& p_i,
-                              const ExecViewUnmanaged<      Scalar[NUM_LEV  ]>& pi) const
+                              const InputProvider& dp,
+                              const ExecView<PT[NUM_LEV_P],Props...>& p_i,
+                              const ExecView<PT[NUM_LEV  ],Props...>& pi) const
   {
     p_i(0)[0] = m_hvcoord.hybrid_ai0*m_hvcoord.ps0;
     ColumnOps::column_scan_mid_to_int<true>(kv,dp,p_i);
     ColumnOps::compute_midpoint_values(kv,p_i,pi);
   }
 
+  template<typename InputProvider, typename PT, typename... Props>
   KOKKOS_FUNCTION
   void get_temperature (const KernelVariables& kv,
                         const EquationOfState& eos,
                         const bool use_moisture,
-                        const ExecViewUnmanaged<const Scalar[NUM_LEV]>& dp,
-                        const ExecViewUnmanaged<const Scalar[NUM_LEV]>& exner,
-                        const ExecViewUnmanaged<const Scalar[NUM_LEV]>& vtheta_dp,
-                        const ExecViewUnmanaged<const Scalar[NUM_LEV]>& qv,
-                        const ExecViewUnmanaged<Scalar[NUM_LEV]>& Rstar,
-                        const ExecViewUnmanaged<Scalar[NUM_LEV]>& T) const {
+                        const InputProvider& dp,
+                        const InputProvider& exner,
+                        const InputProvider& vtheta_dp,
+                        const InputProvider& qv,
+                        const ExecView<PT[NUM_LEV],Props...>& Rstar,
+                        const ExecView<PT[NUM_LEV],Props...>& T) const {
     using namespace PhysicalConstants;
     get_R_star(kv, use_moisture, qv, Rstar);
     Kokkos::parallel_for(
