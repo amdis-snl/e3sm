58aeeaf31805a069f82f818143c1b9c1389efac9
Luca Bertagna <lbertag@sandia.gov>
2026-04-13 10:48:36 -0600

    Homme: move config string method to Config.hpp

--- components/homme/src/share/cxx/Config.hpp
+++ components/homme/src/share/cxx/Config.hpp
@@ -50,4 +50,59 @@
 # define ConstExceptGnu const
 #endif
 
+#include <ekat_arch.hpp>
+
+#ifdef HOMMEXX_ENABLE_FAD_TYPES
+// Disable view specializations
+#define SACADO_DISABLE_FAD_VIEW_SPEC
+#include <Sacado.hpp>
+#endif
+
+#include <string>
+
+namespace Homme {
+
+inline std::string homme_config_settings_string ()
+{
+  std::string s;
+  // Print configure-time settings.
+#ifdef HOMMEXX_SHA1
+  s += "HOMMEXX SHA1: " + std::string(HOMMEXX_SHA1) + "\n";
+#endif
+  s += "HOMMEXX VECTOR_SIZE: " + std::to_string(HOMMEXX_VECTOR_SIZE) + "\n";
+  s += "HOMMEXX Active AVX settings: " + ekat::active_avx_string () + "\n";
+  s += "HOMMEXX MPI_ON_DEVICE: " + std::string(HOMMEXX_MPI_ON_DEVICE ? "yes" : "no") + "\n";
+#ifdef HOMMEXX_CUDA_SHARE_BUFFER
+  s += "HOMMEXX CUDA_SHARE_BUFFER: on\n";
+#else
+  s += "HOMMEXX CUDA_SHARE_BUFFER: off\n";
+#endif
+  s += "HOMMEXX CUDA_(MIN/MAX)_WARP_PER_TEAM: "
+     + std::to_string(HOMMEXX_CUDA_MIN_WARP_PER_TEAM) + "/"
+     + std::to_string(HOMMEXX_CUDA_MAX_WARP_PER_TEAM) + "\n";
+#ifndef HOMMEXX_NO_VECTOR_PRAGMAS
+  s += "HOMMEXX has vector pragmas\n";
+#else
+  s += "HOMMEXX doesn't have vector pragmas\n";
+#endif
+#ifdef HOMMEXX_ENABLE_FAD_TYPES
+  s += "HOMMEXX has Sacado's FAD type support. " + Sacado_Version() + "\n";
+  s += "HOMMEXX DP_SFAD length: " + std::to_string(HOMMEXX_DP_SFAD_SIZE) + "\n";
+#else
+  s += "HOMME does NOT have Sacado's FAD type support\n";
+#endif
+
+#ifdef HOMMEXX_CONFIG_IS_CMAKE
+  s += "HOMMEXX configured with CMake\n";
+# ifdef HAVE_CONFIG_H
+  s += "HOMMEXX has config.h.c\n";
+# endif
+#else
+  s += "HOMMEXX provided best default values in Config.hpp\n";
+#endif
+  return s;
+}
+
+} // namespace Homme
+
 #endif // HOMMEXX_CONFIG_HPP
